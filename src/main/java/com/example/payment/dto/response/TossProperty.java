92ff6a19aa3051b5758a6667929e022d1ea304e6
yumip1997 <yumip1997@gmail.com>
2024-04-29 16:58:01 +0900

    토스 개선 사항 적용 중

--- src/main/java/com/example/payment/dto/response/TossProperty.java
+++ src/main/java/com/example/payment/dto/response/TossProperty.java
@@ -1,7 +1,11 @@
 package com.example.payment.dto.response;
 
+import lombok.Getter;
+
+@Getter
 public class TossProperty {
 
+    private String mid;
     private String clientKey;
     private String sercretKey;
     private String customerKey;
--- src/main/java/com/example/payment/manager/PgPropertyManager.java
+++ src/main/java/com/example/payment/manager/PgPropertyManager.java
@@ -1,10 +1,40 @@
 package com.example.payment.manager;
 
 import com.example.payment.dto.response.TossProperty;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@ConfigurationProperties(prefix = "pg")
+@RequiredArgsConstructor
 public class PgPropertyManager {
 
-    public TossProperty lookupTossProperty(String pgMid){
-        return new TossProperty();
+    private final List<TossProperty> tossPropertyList;
+
+    private Map<String, TossProperty> tossPropertyMap;
+
+    @PostConstruct
+    void initMap(){
+        tossPropertyMap = convertMapByMid(tossPropertyList, TossProperty::getMid);
+    }
+
+    private <T> Map<String, T> convertMapByMid(List<T> list, Function<T, String> midMapper){
+        if(CollectionUtils.isEmpty(list)){
+            return Collections.emptyMap();
+        }
+
+        return list.stream()
+                .collect(Collectors.toMap(midMapper, Function.identity(), (o1,o2) -> o1));
+    }
+
+    public TossProperty lookupTossProperty(String mid){
+        return tossPropertyMap.get(mid);
     }
 }
